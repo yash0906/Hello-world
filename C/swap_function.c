4a7e8ce5583f9956357a3f63eda977d5332f360b
yash bhansali <43386041+yash0906@users.noreply.github.com>
2018-10-22 23:24:24 +0530

    swap without using any variable

--- C/swap_function.c
+++ C/swap_function.c
@@ -1,22 +1,17 @@
+ #include<stdio.h>
+   int main()
+   {
+       int a,b;
+       printf("ENTER TWO NUMBERS\n");
+       printf("a = ");
+       scanf("%d",&a);
+       printf("b = ");
+       scanf("%d",&b);
+       a=a+b;
+       b=a-b;
+       a=a-b;
+       printf("AFTER SWAPPING\n");
+       printf("a = %d\nb = %d\n",a,b);
+       return 0;
+  }
 
-/* Swaping int adress */
-void	swap(int *n1, int *n2)
-{
-  int tmp = 0;
-
-  tmp = *n1;
-  *n1 = *n2;
-  *n2 = tmp;
-}
-
-/* #include <stdio.h> */
-
-/* int main() */
-/* { */
-/*   int a = 1; */
-/*   int b = -1; */
-
-/*   printf("Befor swap: %d, %d\n", a, b); */
-/*   swap(&a, &b); */
-/*   printf("after swap: %d, %d\n", a, b);   */
-/* } */
